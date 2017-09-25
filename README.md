1a668d67877a16c09d6ffe687e07def07d10d2f0
Sandeep Anand <anandjairam@ufl.edu>
2017-09-24 19:00:35 -0700

    2nd C - [9-24]

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # happyBot
-## Runs as Below in the Anaconda Environment/ Anaconda Prompt 
+## Runs as Below in the Anaconda Environment/ Anaconda Prompt
  ```
-(C:\Users\sanan\Anaconda3) C:\Public\Code\happyBot>python main.py
-Enter your sentence and I will respond !
-This is where I will be
-This is where I will be
+ (C:\Users\sanan\Anaconda3) C:\Public\Code\happyBot>python main.py
+ Enter your greeting and I will respond !
+ Good Morning
+ No Comment
  ```
--- main.py
+++ main.py
@@ -2,11 +2,42 @@
 happyBot
 '''
 import spacy
+# Sentences we'll respond with if the user greeted us
+
+#######
+# Considering a small subset and then go ahead with a proper database
+#######
+GREETING_KEYWORDS = ["hello", "hi", "greetings", "sup",
+                    "what's up","Hey", "Hey There", "Good Morning",
+                    "Good Afternoon", "Good Evening",
+                    "What’s new?", "What’s going on?",
+                    "How’s everything ?", "How are things?", "How’s life?",
+                    "Hi There!"]
+
+GREETING_RESPONSES = ["'sup bro", "hey", "Hi There", "hey you get my snap?",
+                        "There is a magic in the air", "No Comment",
+                        "How you doing", "Never Mind", "Close call",
+                        "There you are!"]
+
+# Do some Postprocessing
+# Remove the punctuations and get the string into a lower case and get the numbers out
+import string
+import re
+import random
+
+def convert(s):
+    s=s.translate(str.maketrans('','',string.punctuation))
+    s=s.lower()
+    s=re.sub(r'(\d+)','',s, flags=re.I)
+    s=re.sub(r'[^a-z]',"",s,flags=re.I)
+    return s
+
+
 class main(object):
     """docstring for main."""
     def __init__(self):
         super(main, self).__init__()
-        self.sentence = input("Enter your sentence and I will respond ! \n")
+        self.sentence = input("Enter your greeting and I will respond ! \n")
 
     def manipulate(self):
         nlp = spacy.load('en')
@@ -14,5 +45,13 @@ class main(object):
         for token in doc:
             print(token)
 
+    def greeting(self):
+        greeting_keys = []
+        for word in GREETING_KEYWORDS:
+            greeting_keys.append(convert(word))
+
+        if convert(self.sentence) in greeting_keys:
+            return print(random.choice(GREETING_RESPONSES))
+
 obj = main()
-obj.manipulate()
+obj.greeting()
