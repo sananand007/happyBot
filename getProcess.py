575ff2be0a2637157fa15fc522492e69dc4961e4
Sandeep Anand <anandjairam@ufl.edu>
2017-10-27 09:07:41 -0700

    Add files via upload
    Latest - n-gram function added

--- getProcess.py
+++ getProcess.py
@@ -3,8 +3,10 @@ Getting a shorter Python Text file
 - helpers:
 - https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
 - https://stackoverflow.com/questions/33726361/counting-the-number-of-unique-words-in-a-list
-
+- https://stackoverflow.com/questions/9394803/python-combine-two-for-loops
+- http://locallyoptimal.com/blog/2013/01/20/elegant-n-gram-generation-in-python/
 - [sA -10/24/2017]    Added Pickle functionality , Currently using the en_US.news.txt and filtering to 5% of the data only
+- [sA -10/27/2017]    Extract Bigrams/n-grams as a list of tuples
 '''
 import os
 import nltk,re,string
@@ -18,10 +20,10 @@ class parsecorpus(object):
         super(parsecorpus, self).__init__()
 
     def filtercorpus(self, path1, path2, path3, filtersize):
-        #f1 = open(path1,'r+',encoding="utf-8")
-        #f2 = open(path2,'w+',encoding="utf-8")
-        f1 = open(path1,'r+')
-        f2 = open(path2,'w+')
+        f1 = open(path1,'r+',encoding="utf-8")
+        f2 = open(path2,'w+',encoding="utf-8")
+        #f1 = open(path1,'r+')
+        #f2 = open(path2,'w+')
         #f3 = open(path3,'wb')
         x = f1.readlines()
 
@@ -57,6 +59,11 @@ class parsecorpus(object):
         #return tokensperline_cleaned.append
         return tokenized
 
+    def ngram(self, sentencelist, type):
+        temp = [list(zip(*[s[i:] for i in range(type)])) for s in sentencelist]
+        res = [tup for x1 in temp for tup in x1]
+        return res
+
     def uniquewords(self, tokens):
         uniquewords = set()
         for word in tokens:
@@ -67,18 +74,19 @@ class parsecorpus(object):
 
 obj = parsecorpus()
 
-#path1 = "C://Public//Data-Science[JHU-Coursera]//project_nlp//final//en_US//en_US.news.txt"
-#path2 = "C://Public//Data-Science[JHU-Coursera]//project_nlp//final//en_US//en_US_new_flt.txt"
-
-path1= "C://Dropbox//Delete_Me//nlp//en_US.news.txt"
-path2= "C://Dropbox//Delete_Me//nlp//en_US_new_flt.txt"
-path3= "C://Dropbox//Delete_Me//nlp//en_US_new_flt.pkl"
+path1 = "C://Public//Data-Science[JHU-Coursera]//project_nlp//final//en_US//en_US.news.txt"
+path2 = "C://Public//Data-Science[JHU-Coursera]//project_nlp//final//en_US//en_US_new_flt.txt"
+path3 = "C://Public//Data-Science[JHU-Coursera]//project_nlp//final//en_US//en_US_new_flt.pkl"
+#path1= "C://Dropbox//Delete_Me//nlp//en_US.news.txt"
+#path2= "C://Dropbox//Delete_Me//nlp//en_US_new_flt.txt"
+#path3= "C://Dropbox//Delete_Me//nlp//en_US_new_flt.pkl"
 
 start = time.clock()
-filtersize = 0.05 #user input , change to make the filter bigger or smaller for the corpus
+filtersize = 0.03 #user input , change to make the filter bigger or smaller for the corpus
 res = obj.filtercorpus(path1, path2, path3, filtersize)
 cleanedLines = obj.clean1(res)
 cleaned_tokens = obj.tokenize(cleanedLines)
 uniqWords = obj.uniquewords(cleaned_tokens)
+ngrams = obj.ngram(cleaned_tokens, 2)
 print("Get the Unique Words",uniqWords)
 print("time taken for filtering with filtersize = {:+f}".format(filtersize), time.clock()-start)
